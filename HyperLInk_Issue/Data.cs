11f5322e1b6b7d2e120b5c0c0463a6bfaaca77da
kiselev_vl <kiselev_vl@mail.ru>
2020-08-21 15:55:48 +0700

    Add project files.

---
+++ HyperLInk_Issue/Data.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HyperLInk_Issue
+{
+    class Data
+    {
+        public static List<Uri> List { get; } = new List<Uri>();
+        static Data()
+        {
+            List.Add(new Uri("mailto:gillian.nader@yahoo.com"));
+            List.Add(new Uri("mailto:brook_jast@gmail.com"));
+            List.Add(new Uri("mailto:douglas@yahoo.com"));
+            List.Add(new Uri("mailto:grant.grady@gmail.com"));
+            List.Add(new Uri("mailto:joannie@hotmail.com"));
+            List.Add(new Uri("mailto:name@yahoo.com"));
+            List.Add(new Uri("mailto:mathilde.larkin@yahoo.com"));
+            List.Add(new Uri("mailto:karina_vonrueden@yahoo.com"));
+            List.Add(new Uri("mailto:gus@yahoo.com"));
+            List.Add(new Uri("mailto:bradley.carroll@gmail.com"));
+            List.Add(new Uri("mailto:wilburn.schaefer@yahoo.com"));
+            List.Add(new Uri("mailto:guido@yahoo.com"));
+            List.Add(new Uri("mailto:dana@yahoo.com"));
+            List.Add(new Uri("mailto:mariela.rutherford@gmail.com"));
+            List.Add(new Uri("mailto:jany_jacobs@yahoo.com"));
+            List.Add(new Uri("mailto:madie.padberg@yahoo.com"));
+            List.Add(new Uri("mailto:harry@yahoo.com"));
+            List.Add(new Uri("mailto:lavada@gmail.com"));
+            List.Add(new Uri("mailto:gisselle_becker@gmail.com"));
+            List.Add(new Uri("mailto:rhiannon.grimes@yahoo.com"));
+            List.Add(new Uri("mailto:amya_grimes@hotmail.com"));
+            List.Add(new Uri("mailto:norbert.beier@yahoo.com"));
+            List.Add(new Uri("mailto:darian@hotmail.com"));
+            List.Add(new Uri("mailto:kaley@yahoo.com"));
+            List.Add(new Uri("mailto:nona@hotmail.com"));
+            List.Add(new Uri("mailto:geo.pacocha@gmail.com"));
+            List.Add(new Uri("mailto:marley.lueilwitz@gmail.com"));
+            List.Add(new Uri("mailto:gay.johns@gmail.com"));
+            List.Add(new Uri("mailto:baron_vandervort@hotmail.com"));
+            List.Add(new Uri("mailto:yasmeen_howell@hotmail.com"));
+            List.Add(new Uri("mailto:lina@hotmail.com"));
+            List.Add(new Uri("mailto:river@gmail.com"));
+            List.Add(new Uri("mailto:felton.hyatt@hotmail.com"));
+            List.Add(new Uri("mailto:torrance@yahoo.com"));
+            List.Add(new Uri("mailto:liana@hotmail.com"));
+            List.Add(new Uri("mailto:jerel@gmail.com"));
+            List.Add(new Uri("mailto:billy@gmail.com"));
+            List.Add(new Uri("mailto:ulices@gmail.com"));
+            List.Add(new Uri("mailto:lane_klein@hotmail.com"));
+            List.Add(new Uri("mailto:zaria@hotmail.com"));
+            List.Add(new Uri("mailto:elisabeth_hauck@yahoo.com"));
+            List.Add(new Uri("mailto:sammy_christiansen@yahoo.com"));
+            List.Add(new Uri("mailto:kendrick.kautzer@hotmail.com"));
+            List.Add(new Uri("mailto:edgar@hotmail.com"));
+            List.Add(new Uri("mailto:newell@hotmail.com"));
+            List.Add(new Uri("mailto:freda_muller@gmail.com"));
+            List.Add(new Uri("mailto:geo@yahoo.com"));
+            List.Add(new Uri("mailto:heidi.bahringer@gmail.com"));
+            List.Add(new Uri("mailto:natasha@gmail.com"));
+            List.Add(new Uri("mailto:giovanni@gmail.com"));
+
+        }
+    }
+}
